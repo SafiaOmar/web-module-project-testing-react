6d84942ac0d913579747e5b01854cec20eddf72c
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-12-14 07:45:39 -0500

    code grade not working

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,39 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={{}} />)
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{ render(<Episode episode={{
+    id: 1,
+    image: 'https://static.tvmaze.com/uploads/images/medium_landscape/204/510098.jpg',
+    name: '"Chapter Four: The Body"',
+    season: 1,
+    number: 4,
+    summary: "Refusing to believe Will is dead, Joyce tries to connect with her son. The boys give Eleven a makeover in order to connect with Will. Nancy and Jonathan form an unlikely alliance.",
+    runtime: 51
+}} />)
 
-test("renders default image when image is not defined", ()=>{});
+const summary = screen.queryByText(/Refusing to believe Will is dead, Joyce tries to connect with her son. The boys give Eleven a makeover in order to connect with Will. Nancy and Jonathan form an unlikely alliance/i)
+
+expect(summary).toBeInTheDocument()
+
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={{
+        id: 1,
+        image: null,
+        name: '"Chapter Four: The Body"',
+        season:1,
+        number: 4,
+        summary: "Refusing to believe Will is dead, Joyce tries to connect with her son. The boys give Eleven a makeover in order to connect with Will. Nancy and Jonathan form an unlikely alliance.",
+        runtime: 51
+    }} />)
+
+    const img = screen.queryByRole('img')
+    console.log('here', img)
+
+    expect(img).toHaveProperty('src','https://i.ibb.co/2FsfXqM/stranger-things.png')
+});
